33df3a1cb1842992010e74b830e9ebf1068f62eb
Stephanie Coy <scoy028@gmail.com>
2018-11-04 16:25:18 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
-function forLoop(anArr) {
-  for (let i = 0; i < 25; i++) {
-    if (i === 1) {
-      anArr.push("I am 1 strange loop.");
-    } else {
-      anArr.push(`I am ${i}  strange loops.`);
-    }
-  }
-  return anArr;
-}
+// function forLoop(anArr) {
+//   for (let i = 0; i < 25; i++) {
+//     if (i === 1) {
+//       anArr.push("I am 1 strange loop.");
+//     } else {
+//       anArr.push(`I am ${i}  strange loops.`);
+//     }
+//   }
+//   return anArr;
+// }
