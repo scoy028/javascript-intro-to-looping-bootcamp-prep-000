d99f4597ab148fa954dc42c42bb24c0abcb52b74
Stephanie Coy <scoy028@gmail.com>
2018-11-04 16:27:21 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ function forLoop(anArr) {
     if (i === 1) {
       anArr.push("I am 1 strange loop.");
     } else {
-      anArr.push(`"I am ${i}  strange loop${i === 0 ? '' : 's'}."`);
+      anArr.push(`I am ${i}  strange loop${i === 0 ? '' : 's'}.`);
     }
   }
   return anArr;
